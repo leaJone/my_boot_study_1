3f9b6bf62ccc5230cdc345e1d380ded966b1d529
lijing <lij@alasga.cn>
2019-04-25 16:22:34 +0800

    初始化

---
+++ src/main/java/com/xxmy/boot/neteasy_one/NeteasyOneApplication.java
@@ -0,0 +1,13 @@
+package com.xxmy.boot.neteasy_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class NeteasyOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeteasyOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xxmy/boot/neteasy_one/NeteasyOneApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xxmy.boot.neteasy_one;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NeteasyOneApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
