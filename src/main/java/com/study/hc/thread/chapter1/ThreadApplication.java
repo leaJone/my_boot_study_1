45bf562a9d91f0545e026cd6e0301c3f08f1b1b8
lijing <lij@alasga.cn>
2019-04-25 16:56:12 +0800

    线程学习

---
+++ src/main/java/com/study/hc/thread/chapter1/ThreadApplication.java
@@ -0,0 +1,13 @@
+package com.study.hc.thread.chapter1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadApplication.class, args);
+    }
+
+}
